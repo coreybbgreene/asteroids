7cde631351914e89cd72bdb4d926730e878cb889
Corey Greene <corey.greene@ucdenver.edu>
2026-06-11 15:04:40 -0600

    Initial commit

---
+++ constants.py
@@ -0,0 +1,8 @@
+# Title: constants.py
+# Author: Corey Greene
+# Date Created: 11 June, 2026
+# Last Update: 11 June, 2026
+# Description: A module containing all constants used within the Asteroid game.
+
+SCREEN_WIDTH = 1280
+SCREEN_HEIGHT = 720
---
+++ main.py
@@ -0,0 +1,17 @@
+# Title: main.py
+# Author: Corey Greene
+# Date Created: 11 June, 2026
+# Last Update: 11 June, 2026
+# Description: Asteroid game for Boot.dev
+
+import pygame
+from constants import SCREEN_WIDTH, SCREEN_HEIGHT
+
+def main():
+    print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+
+
+if __name__ == "__main__":
+    main()
