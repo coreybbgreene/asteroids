0c8a17a16b78b4ee1cbdd3be9e658f97c5d1af84
Corey Greene <corey.greene@ucdenver.edu>
2026-06-11 15:24:27 -0600

    Added FPS(60)

--- main.py
+++ main.py
@@ -9,8 +9,14 @@ from logger import log_state
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 
 def main():
+    # Initialize pygame and create the game window
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    # Set Clock to 60 FPS
+    clock = pygame.time.Clock()
+    dt = 0.0
+
 
     while True:
         log_state()
@@ -19,6 +25,8 @@ def main():
                 return
         screen.fill("black")
         pygame.display.flip()
+        clock.tick(60)
+        dt = clock.tick(60) / 1000
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
